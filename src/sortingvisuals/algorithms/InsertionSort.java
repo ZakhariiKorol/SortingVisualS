ec0b21ab7c9cdee6aa5d724c9bfaec018e1376b7
ZakhariiKorol <zaharkorol2001@gmail.com>
2026-03-31 19:53:05 +0200

    Insertion Sort not implemented yet

--- src/sortingvisuals/algorithms/InsertionSort.java
+++ src/sortingvisuals/algorithms/InsertionSort.java
@@ -14,8 +14,24 @@ public class InsertionSort implements SortAlgorithm {
     private int i = 0;
     private int j = 0;
     
+    private Timeline timeline;
+    
     @Override
     public void sort (int[] array, Rectangle[] bars, Pane pane) {
-        //...
+        
+        i = 0;
+        j = 0;
+        
+        timeline = new Timeline(new KeyFrame(Duration.millis(600), e -> {
+            //
+        }));
+        
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.stop();
+    }
+    
+    @Override
+    public String toString() {
+        return "Insertion Sort";
     }
 }
